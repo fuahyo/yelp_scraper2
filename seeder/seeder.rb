c2292c194a1604798ae603f0d4ba5e40e9c73cdb
Mikhail <saintprog@gmail.com>
2019-12-25 19:07:35 +0300

    fix seeder

--- seeder/seeder.rb
+++ seeder/seeder.rb
@@ -13,7 +13,7 @@ seed_data.each do |code, coords|
   (coords[:sw_corner][:lat].floor..coords[:ne_corner][:lat].ceil).each do |lat|
     (coords[:sw_corner][:long].floor..coords[:ne_corner][:long].ceil).each do |long|
       pages << {
-        url: "https://www.yelp.com/search/snippet?cflt=restaurants&l=g:#{lat.to_f},#{long.to_f},#{lat.to_f-1},#{long.to_f-1}&start=0",
+        url: "https://www.yelp.com/search/snippet?cflt=restaurants&l=g:#{long.to_f},#{lat.to_f},#{long.to_f-1},#{lat.to_f-1}&start=0",
         page_type: 'locations',
         headers: {
           "User-Agent" => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Safari/537.36",
