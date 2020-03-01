8abffd2d08bb0cf6390fdcf4017aa33274f95d18
Mikhail <saintprog@gmail.com>
2020-03-01 18:48:06 +0300

    some fixes

--- seeder/seeder.rb
+++ seeder/seeder.rb
@@ -17,8 +17,8 @@ seed_data.each do |code, coords|
         page_type: 'locations',
         fetch_type: "browser",
         headers: {
-          "User-Agent" => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Safari/537.36",
-          "Accept" => "*/*"
+          "User-Agent" => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.116 Safari/537.36",
+          "Accept" => "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"
         },
         http2: true,
         vars: {
