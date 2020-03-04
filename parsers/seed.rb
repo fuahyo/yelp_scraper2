1858946e41672c4682220017dd2a6e891fc996ad
Mikhail <saintprog@gmail.com>
2020-03-04 15:25:10 +0300

    use browser fetcher and change workflow

---
+++ parsers/seed.rb
@@ -0,0 +1,66 @@
+if page['failed_response_status_code']
+  puts 'refetch seed'
+  refetch page['gid']
+end
+
+parsable = true
+
+# html = Nokogiri::HTML(content)
+# json = JSON.parse(content) rescue nil
+html = Nokogiri::HTML(content)
+
+parsable = false if !!html.at('div.no-results')
+parsable = false if !!page.at('p.lemon--p__373c0__3Qnnj.text__373c0__2pB8f.text-color--normal__373c0__K_MKN:contains("Suggestions for improving the results")')
+
+if json.nil?
+  puts 'refetch page'
+  refetch page['gid']
+end
+
+if parsable
+
+  if page['vars']['initial']
+    last_page = html.at('div[aria-label="Pagination navigation"] span:contains("of")').text.scan(/1 of (\d+)/).first.first.to_i rescue 1
+    (2..last_page).each do |page_num|
+      link = page['url'].gsub('start=0', "start=#{page_num*30-30}")
+      pages << {
+        url: link,
+        page_type: 'seed',
+        fetch_type: "browser",
+        headers: {
+          "User-Agent" => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.116 Safari/537.36",
+          "Accept" => "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"
+        },
+        http2: true,
+        vars: {
+          country: page['vars']['country'],
+        }
+      }
+    end
+  end
+
+  # page.search('li.domtags--li__373c0__3TKyB.list-item__373c0__M7vhU h3:has(a)').each do |item|
+  # page.search('li.lemon--li__373c0__1r9wz.border-color--default__373c0__2oFDT h3:has(p)').each do |item|
+  html.search('li.lemon--li__373c0__1r9wz a:has(img).lemon--a__373c0__IEZFH').each do |item|
+    uri = item['href']
+    link = "https://www.yelp.com#{uri}"
+    if link =~ /redirect_url/
+      link = URI.decode(link.scan(/redirect_url=(http.+?)&/).first.first)
+    end
+    pages << {
+      url: link,
+      page_type: 'restaurant',
+      fetch_type: "browser",
+      headers: {
+        "User-Agent" => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.116 Safari/537.36",
+        "Accept" => "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"
+      },
+      http2: true,
+      vars: {
+        country: page['vars']['country'],
+        position: (item.at('text()').text().to_i rescue 0)
+      }
+    }
+  end
+
+end
--- seeder/seeder.rb
+++ seeder/seeder.rb
@@ -12,10 +12,24 @@ seed_data = {
 seed_data.each do |code, coords|
   (coords[:sw_corner][:lat].floor..coords[:ne_corner][:lat].ceil).each do |lat|
     (coords[:sw_corner][:long].floor..coords[:ne_corner][:long].ceil).each do |long|
+      # pages << {
+      #   url: "https://www.yelp.com/search/snippet?cflt=restaurants&l=g:#{long.to_f},#{lat.to_f},#{long.to_f-1},#{lat.to_f-1}&start=0",
+      #   page_type: 'locations',
+      #   # fetch_type: "browser",
+      #   headers: {
+      #     "User-Agent" => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.116 Safari/537.36",
+      #     "Accept" => "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"
+      #   },
+      #   http2: true,
+      #   vars: {
+      #     country: code,
+      #     initial: true
+      #   }
+      # }
       pages << {
-        url: "https://www.yelp.com/search/snippet?cflt=restaurants&l=g:#{long.to_f},#{lat.to_f},#{long.to_f-1},#{lat.to_f-1}&start=0",
-        page_type: 'locations',
-        # fetch_type: "browser",
+        url: "https://www.yelp.com/search?start=0&cflt=restaurants&l=g:#{long.to_f},#{lat.to_f},#{long.to_f-1},#{lat.to_f-1}",
+        page_type: 'seed',
+        fetch_type: "browser",
         headers: {
           "User-Agent" => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.116 Safari/537.36",
           "Accept" => "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"
