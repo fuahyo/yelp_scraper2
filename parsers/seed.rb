05936f4772e8b95f034ecd897535a89fb0d0435c
Mikhail <saintprog@gmail.com>
2021-03-17 15:12:25 +0300

    little fix

--- parsers/seed.rb
+++ parsers/seed.rb
@@ -45,7 +45,8 @@ if parsable
   # html.search('li.lemon--li__373c0__1r9wz a:has(img).lemon--a__373c0__IEZFH').each do |item|
   # html.search('li.lemon--li__09f24__1r9wz a:has(img).lemon--a__09f24__IEZFH').each do |item|
   # html.search('li.border-color--default__09f24__R1nRO a:has(img).link__09f24__1kwXV').each do |item|
-  html.search('li.border-color--default__09f24__R1nRO a:has(img).link__09f24__1MGLa').each do |item|
+  # html.search('li.border-color--default__09f24__R1nRO a:has(img).link__09f24__1MGLa').each do |item|
+  html.search('li.border-color--default__09f24__R1nRO a:has(img).photo-box-link__09f24__28L0f').each do |item|
     uri = item['href']
     link = "https://www.yelp.com#{uri}"
     if link =~ /redirect_url/
