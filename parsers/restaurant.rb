ad6cafbc78c340710928dff35f6a09489bbdb43b
ibnuthoriqh <ibnuthoriqh@gmail.com>
2022-11-01 08:34:50 +0700

    fix

--- parsers/restaurant.rb
+++ parsers/restaurant.rb
@@ -1,5 +1,5 @@
 parsable = true
-if page['status'] == 'refetch_failed'
+if page['refetch_count'] == 3
   refetch_count = (page['vars']['refetch_count'].nil?)? 1 : page['vars']['refetch_count'] + 1
   if refetch_count < 2
     pages << {
