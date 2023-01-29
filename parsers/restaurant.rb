861bb5c3fe24fd6403ac86bc1865b61f6732ce18
justinever9 <justinever9@gmail.com>
2023-01-29 20:04:38 +0700

    test

--- parsers/restaurant.rb
+++ parsers/restaurant.rb
@@ -5,11 +5,11 @@ if false
     pages << {
       url: page['url'],
       page_type: 'restaurant',
-      fetch_type: "browser",
-      driver: {
-        name: "refetch",
-        enable_images: true,
-      },
+      # fetch_type: "browser",
+      # driver: {
+      #   name: "refetch",
+      #   enable_images: true,
+      # },
       http2: true,
       vars: {
         country: page['vars']['country'],
@@ -317,11 +317,11 @@ else
         pages << {
           url: "https://www.yelp.com#{related}",
           page_type: 'restaurant',
-          fetch_type: "browser",
-          headers: {
-            "User-Agent" => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.116 Safari/537.36",
-            "Accept" => "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"
-          },
+          # fetch_type: "browser",
+          # headers: {
+          #   "User-Agent" => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.116 Safari/537.36",
+          #   "Accept" => "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"
+          # },
           http2: true,
           vars: {
             country: page['vars']['country'],
