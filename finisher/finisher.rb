833b582d8cad1b15de8e8bfc7222fda062c14fe0
justinever9 <justinever9@gmail.com>
2023-01-31 02:37:39 +0700

    revert finisher

--- finisher/finisher.rb
+++ finisher/finisher.rb
@@ -10,7 +10,7 @@ while true
     '$orderby' => [{'_id' => 1}]
   }
   records = find_outputs("locations_#{country_code.downcase}", query, 1, per_page)
-  records2 = find_outputs("locations", query, 1, per_page)
+  # records2 = find_outputs("locations", query, 1, per_page)
 
   records.each do |location|
     # location['free_field'] = {}
@@ -23,19 +23,6 @@ while true
     outputs << location
 
     save_outputs(outputs) if outputs.count > 99
-  end
-
-  records2.each do |location|
-    # location['free_field'] = {}
-    cuisines = {}
-    location['cuisine_name'].each_with_index do |cuisine, index|
-        cuisines["cuisine#{index + 1}"] = cuisine
-    end rescue {}
-    
-    location['cuisine_name'] = cuisines
-    outputs << location
-
-    # save_outputs(outputs) if outputs.count > 99
   end
 
   break if records.nil? || records.count < 1
