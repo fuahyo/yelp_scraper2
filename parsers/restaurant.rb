391cb6eb941c3f1f56d1f852567aed3042d9c316
justinever9 <justinever9@gmail.com>
2023-02-01 02:43:00 +0700

    fix scraper country

--- parsers/restaurant.rb
+++ parsers/restaurant.rb
@@ -263,7 +263,7 @@ else
 
       cuisines = nil if cuisines.empty?
       
-      if json['address']["addressCountry"] = page['vars']['country']
+      if json['address']["addressCountry"] == page['vars']['country']
         location = {
           # _collection: "locations_#{page['vars']['country'].downcase}",
           _collection: 'locations',
