9025c61e62a5a32c83b04dd3cf3f26195daeea9d
justinever9 <justinever9@gmail.com>
2023-02-08 18:58:43 +0700

    disable related search for reaprse

--- parsers/restaurant.rb
+++ parsers/restaurant.rb
@@ -306,22 +306,22 @@ else
         outputs << location
       end
 
-      html.search('section:contains("People Also Viewed") a[href]').map{|a| a['href']}.each do |related|
-        pages << {
-          url: "https://www.yelp.com#{related}",
-          page_type: 'restaurant',
-          fetch_type: "browser",
-          headers: {
-            "User-Agent" => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.116 Safari/537.36",
-            "Accept" => "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"
-          },
-          http2: true,
-          vars: {
-            country: page['vars']['country'],
-            position: (0)
-          }
-        }
-      end
+      # html.search('section:contains("People Also Viewed") a[href]').map{|a| a['href']}.each do |related|
+      #   pages << {
+      #     url: "https://www.yelp.com#{related}",
+      #     page_type: 'restaurant',
+      #     fetch_type: "browser",
+      #     headers: {
+      #       "User-Agent" => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.116 Safari/537.36",
+      #       "Accept" => "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"
+      #     },
+      #     http2: true,
+      #     vars: {
+      #       country: page['vars']['country'],
+      #       position: (0)
+      #     }
+      #   }
+      # end
     end
   end
 end
