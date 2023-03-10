2397855a28ccf980e83ae1d84e078374d89ef954
justinever9 <justinever9@gmail.com>
2023-03-10 19:02:24 +0700

    update finisher

--- finisher/finisher.rb
+++ finisher/finisher.rb
@@ -10,29 +10,30 @@ while true
   records = find_outputs("locations", query, 1, per_page)
 
   records.each do |location|
-    cuisines = {}
-    location['cuisine_name'].each_with_index do |cuisine, index|
-        cuisines["cuisine#{index + 1}"] = cuisine
-    end rescue {}
+    # cuisines = {}
+    # location['cuisine_name'].each_with_index do |cuisine, index|
+    #     cuisines["cuisine#{index + 1}"] = cuisine
+    # end rescue {}
 
-    unless location['restaurant_delivery_zones'].nil?
-      location['restaurant_delivery_zones'].first['currency'] = ENV['currency_code']
-    end 
+    # unless location['restaurant_delivery_zones'].nil?
+    #   location['restaurant_delivery_zones'].first['currency'] = ENV['currency_code']
+    # end 
     
-    unless location['main_cuisine'].nil?
-      if location['main_cuisine'].include?(',')
-        main_cuisine = location['main_cuisine'].split(',').first
-        location['main_cuisine'] = main_cuisine
-      end
-    end
+    # unless location['main_cuisine'].nil?
+    #   if location['main_cuisine'].include?(',')
+    #     main_cuisine = location['main_cuisine'].split(',').first
+    #     location['main_cuisine'] = main_cuisine
+    #   end
+    # end
     # location['restaurant_post_code'] = nil if location['restaurant_post_code'].nil? || location['restaurant_post_code'].empty?
     
-    unless location['restaurant_post_code'].nil?
-      post_code = location['restaurant_post_code'].to_f
-      location['restaurant_post_code'] = post_code
-    end
-    location['cuisine_name'] = cuisines
+    # unless location['restaurant_post_code'].nil?
+    #   post_code = location['restaurant_post_code'].to_f
+    #   location['restaurant_post_code'] = post_code
+    # end
+    # location['cuisine_name'] = cuisines
 
+    location['free_field'] = nil if location['free_field']['website'].nil?
     outputs << location
 
     save_outputs(outputs) if outputs.count > 99
