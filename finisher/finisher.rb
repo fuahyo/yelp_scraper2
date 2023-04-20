34ff9706d409feff781930618e546dcc092a8753
justinever9 <justinever9@gmail.com>
2023-04-20 14:45:41 +0700

    fix finisher

--- finisher/finisher.rb
+++ finisher/finisher.rb
@@ -10,28 +10,21 @@ while true
   records = find_outputs("locations", query, 1, per_page)
 
   records.each do |location|
-    # cuisines = {}
-    # location['cuisine_name'].each_with_index do |cuisine, index|
-    #     cuisines["cuisine#{index + 1}"] = cuisine
-    # end rescue {}
+    cuisines = {}
+    location['cuisine_name'].each_with_index do |cuisine, index|
+        cuisines["cuisine#{index + 1}"] = cuisine
+    end rescue {}
+    location['cuisine_name'] = cuisines
 
-    # unless location['restaurant_delivery_zones'].nil?
-    #   location['restaurant_delivery_zones'].first['currency'] = ENV['currency_code']
-    # end 
+    unless location['restaurant_delivery_zones'].nil?
+      location['restaurant_delivery_zones'].first['currency'] = ENV['currency_code']
+    end 
     
-    # unless location['main_cuisine'].nil?
-    #   if location['main_cuisine'].include?(',')
-    #     main_cuisine = location['main_cuisine'].split(',').first
-    #     location['main_cuisine'] = main_cuisine
-    #   end
-    # end
-    # location['restaurant_post_code'] = nil if location['restaurant_post_code'].nil? || location['restaurant_post_code'].empty?
-    
-    # unless location['restaurant_post_code'].nil?
-    #   post_code = location['restaurant_post_code'].to_f
-    #   location['restaurant_post_code'] = post_code
-    # end
-    # location['cuisine_name'] = cuisines
+    unless location['restaurant_post_code'].nil?
+      location['restaurant_post_code'] = nil if location['restaurant_post_code'].empty?
+      post_code = location['restaurant_post_code'].to_f
+      location['restaurant_post_code'] = post_code
+    end
 
     unless location['free_field'].nil?
       if location['free_field']['website'].nil? || location['free_field'].empty?
