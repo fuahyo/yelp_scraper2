d5bcb8fd104d1ae8c1b7c78b878adae59d81f8f5
justinever9 <justinever9@gmail.com>
2023-11-30 20:29:43 +0700

    update finisher

--- finisher/finisher.rb
+++ finisher/finisher.rb
@@ -10,30 +10,36 @@ while true
   records = find_outputs("locations", query, 1, per_page)
 
   records.each do |location|
-    # cuisines = {}
-    # location['cuisine_name'].each_with_index do |cuisine, index|
-    #     cuisines["cuisine#{index + 1}"] = cuisine
-    # end rescue {}
-    # location['cuisine_name'] = cuisines
-    if location['main_cuisine'].include?','
-      location['main_cuisine'] = location['main_cuisine'].split(',').first
-  
-    end
-    unless location['restaurant_delivery_zones'].nil?
-      location['restaurant_delivery_zones'].first['currency'] = ENV['currency_code']
-    end 
-    
-    unless location['restaurant_post_code'].nil?
-      location['restaurant_post_code'] = nil if location['restaurant_post_code'].empty?
-    end
-
-    unless location['free_field'].nil?
-      if location['free_field']['website'].nil? || location['free_field'].empty?
-        location['free_field'] = nil
+    tag = []
+    unless location['restaurant_tags'].nil?
+      location['restaurant_tags'].each do |tg|
+        if !tag.include? tg
+          tag.append(tg)
+        end
       end
+      location['restaurant_tags'] = tag
     end
+    # if location['main_cuisine'].include?','
+    #   location['main_cuisine'] = location['main_cuisine'].split(',').first
+  
+    # end
+    # unless location['restaurant_delivery_zones'].nil?
+    #   location['restaurant_delivery_zones'].first['currency'] = ENV['currency_code']
+    # end 
+    
+    # unless location['restaurant_post_code'].nil?
+    #   location['restaurant_post_code'] = nil if location['restaurant_post_code'].empty?
+    # end
 
-    location['restaurant_post_code'] = nil if location['restaurant_post_code'] == 0
+    # unless location['free_field'].nil?
+    #   if location['free_field']['website'].nil? || location['free_field'].empty?
+    #     location['free_field'] = nil
+    #   end
+    # end
+    # location['restaurant_rating'] = location['restaurant_rating'].to_f
+    location['restaurant_rating'] = nil if location['restaurant_rating'] == 0 
+    location['number_of_ratings'] = nil if location['number_of_ratings'] == 0
+
     outputs << location
 
     save_outputs(outputs) if outputs.count > 99
